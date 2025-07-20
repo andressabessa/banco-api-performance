1b7926aa9391f5dce236a1e362afc52c15ddfdfa
Andressa Bessa <andreessabessa@gmail.com>
2025-07-20 12:23:19 -0300

    Adicionando dados de entrada para os testes

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,5 +1,6 @@
 import http from 'k6/http'
 import { sleep, check } from 'k6'
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
 
 
 export const options = {
@@ -10,22 +11,19 @@ export const options = {
     ],
     thresholds: {
         http_req_duration: ['p(90)<3000', 'max<5000'],
-        http_req_failed: ['rate<0.01'], // 1% de falhas
+        http_req_failed: ['rate<0.01']
 
     }
   }
 
   export default function () {
-    const url = 'http://localhost:3000/login';
-    const payload = JSON.stringify({
-      username: 'julio.lima',
-      senha: '123456',
-    })
+    const url = 'http://localhost:3000/login'
+    const payload = JSON.stringify(postLogin)
   
     const params = {
       headers: {
         'Content-Type': 'application/json',
-      },
+      }
     }
   
     const res = http.post(url, payload, params)
